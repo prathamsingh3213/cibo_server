462fdace16302657fdfc4ac5fc7eeadf0f918384
Pratham singh <153312369+prathamsingh3213@users.noreply.github.com>
2024-05-14 17:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,41 +16,39 @@ def recommend_dish():
         def filter_ingredients(df, filter_list):
             filtered_df = df[df['ingredients'].apply(lambda x: all(ingredient in x for ingredient in filter_list))]
             return filtered_df
-        print("Inside the function")
+
         data = request.get_json()
         ingredients = data['ingredients']
-        essentials=data['essentials']
+        # Lowercase the elements of essentials list
+        essentials = [essential.lower() for essential in data.get("essentials", [])]
+
         df1=pd.read_csv("cleaned_file-1.csv.gz",index_col="index")
         df2=pd.read_csv("cleaned_file-2.csv.gz",index_col="index")
         df3=pd.read_csv("cleaned_file-3.csv.gz",index_col="index")
         df=pd.concat([df1,df2,df3])
-
-        result=filter_ingredients(df.copy(),essentials)
-        # result['ingredients']
-
+        result = filter_ingredients(df.copy(), essentials)
 
         tfidf_vectorizer2=TfidfVectorizer(stop_words='english')
         tfidf_matrix2=tfidf_vectorizer2.fit_transform(result['ingredients'])
 
-
+       
         # Use the model for recommendation
         user_idf = tfidf_vectorizer2.transform([ingredients])
         sim_ing = cosine_similarity(user_idf, tfidf_matrix2)
-        li=sorted(list(enumerate(sim_ing[0])),reverse=True,key=lambda x:x[1])[0:5]
-        li
-        indices = [index for index, _ in li]
-        newdf=result.iloc[indices]
-        json=newdf.to_json(orient='records')
+
+        
+
+        
 
         # Use the model for recommendation
-        user_idf = tfidf_vectorizer2.transform([ingredients])
-        sim_ing = cosine_similarity(user_idf, tfidf_matrix2)
-        li=sorted(list(enumerate(sim_ing[0])),reverse=True,key=lambda x:x[1])[0:5]
-        li
+        # user_idf = tfidf_vectorizer.transform([ingredients])
+        # sim_ing = cosine_similarity(user_idf, tfidf_matrix)
+        li = sorted(list(enumerate(sim_ing[0])), reverse=True, key=lambda x: x[1])[0:5]
         indices = [index for index, _ in li]
-        newdf=df.loc[indices]
-        json=newdf.to_json(orient='records')
-        return json
+        newdf = result.iloc[indices]
+        json_data = newdf.to_json(orient='records')
+
+        return json_data
 
     except Exception as e:
         # Log the exception details
